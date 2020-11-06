8dc0ccc222ffe53a0e83d551635ddae0b70daf8c
William Damm <wdamm@powerhrg.com>
2020-11-06 19:27:23 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -38,6 +38,18 @@ for (let i = 0; i < divs.length; i++) {
 }
 
 
+divs = document.querySelectorAll('div');
+ 
+function capture(e) {
+  console.log(this.firstChild.nodeValue.trim() + ' captured');
+}
+ 
+for (let i = 0; i < divs.length; i++) {
+  // set the third argument to `true`!
+  divs[i].addEventListener('click', capture, true);
+}
+
+
 
 
 
