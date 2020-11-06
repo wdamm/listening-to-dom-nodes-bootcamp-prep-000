1a9bb21f7f6674102a06c7a55ba380b203062757
William Damm <wdamm@powerhrg.com>
2020-11-06 19:26:08 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -4,3 +4,9 @@ main.addEventListener('click', function(event) {
   alert('I was clicked!');
 });
 
+const input = document.querySelector('input');
+ 
+input.addEventListener('keydown', function(e) {
+  console.log(e.key);
+});
+
